39588d11768bd38fa315a9437400dcf53864eae2
atalayozmen <atalayozmen21@gmail.com>
2021-10-01 12:35:09 +0300

    updates-2

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
   return (
     <React.Fragment>
       <CartContext.Provider value={{ addCartItems: addCartItems, removeItem: removeItem, emptyArray: emptyArray, cartState: cartState }}>
-        <div style={{ fontFamily: "sans-serif", backgroundColor: "#A9E4D7",width: '100%', paddingBottom: "20px" }}>
+        <div style={{position: "fixed", fontFamily: "sans-serif", backgroundColor: "#A9E4D7",width: '100%', minHeight: '100%', paddingBottom: "20px" }}>
           <Navigation />
           <IntroText />
           <Menu />
